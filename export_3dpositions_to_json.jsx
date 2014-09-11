5de1c57d399cd0d9f94038dc1d049c8aa4e6fd2a
Silvère Maréchal <silvere.marechal@gmail.com>
2014-09-11 23:18:37 +0200

    add 3dcoordinate exporter from AE to JSON

---
+++ export_3dpositions_to_json.jsx
@@ -0,0 +1,46 @@
+
+function getAndWriteData(comp, layer)
+{
+	
+	// open the file
+	var file = new File("positions.json");
+    
+    var numEffects = layer.Effects.numProperties;    
+    
+	if (file.open("w"))
+	{
+        file.writeln('/* 3D positions exported from Export 3DPositions to JSON');		
+		file.writeln('Written by Silvere Marechal for personal AE to CSS 3D Transform workflow');
+		file.writeln('https://github.com/turbodrive');
+        file.writeln('');
+        file.writeln('Based on the exporter by Thomas Apodaca https://github.com/tmaybe */');
+        file.writeln('');
+        file.writeln('{');
+        
+        for(var i = 1; i<=numEffects; i++){
+            
+            var effectLayer = layer.Effects.property(i);
+            var name = effectLayer.name;
+            var pX = effectLayer.property("Position XY").value[0];
+            var pY = effectLayer.property("Position XY").value[1];
+            var pZ = effectLayer.property("Position Z").value;
+            var rX = effectLayer.property("Rotation XY").value[0];
+            var rY = effectLayer.property("Rotation XY").value[1];
+            var rZ = effectLayer.property("RotationZ").value;
+                        
+            if(pX != null && pY != null && pZ != null && rX != null && rY != null && rZ != null){
+                    file.writeln('{name:"'+name+'",x:'+pX+',y:'+pY+',z:'+pZ+',rotationX:'+rX+',rotationY:'+rY+',rotationZ:'+rZ+'},');
+            }
+            
+        }
+        file.writeln('}');
+		file.close();
+	}
+    
+    alert("Export complete");
+}
+
+var comp = app.project.activeItem;
+var layer = comp.selectedLayers[0];
+
+getAndWriteData(comp, layer);
